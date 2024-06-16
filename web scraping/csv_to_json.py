814ca7c48d9a800f3fbceb0710ecf7978f10cb37
Lerne <luisernestoserrras@gmail.com>
2024-06-15 21:29:57 -0400

    test

---
+++ web scraping/csv_to_json.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import json
+
+def csv_to_json(ruta_csv, ruta_json):
+    try:
+        # Leer el archivo CSV en un DataFrame
+        df = pd.read_csv(ruta_csv)
+
+        # Generando el diccionario a partir del DataFrame completo
+        titanic_json = df.to_dict(orient='records')
+
+        # Guardando en archivo Json
+        with open(ruta_json, 'w') as archivo_json:
+            json.dump(titanic_json, archivo_json, indent=4)
+        
+        print("Archivo JSON creado exitosamente.")
+    except Exception as e:
+        print(f"Error al procesar el archivo CSV: {e}")
+#Prueba:
+ruta_csv = 'data/csv/9.16-existencia-de-ganado-vacuno-segun-sexo-y-categorias-en-diciembre-31-.csv'
+ruta_json = 'result.json'
+csv_to_json(ruta_csv,ruta_json)
+
