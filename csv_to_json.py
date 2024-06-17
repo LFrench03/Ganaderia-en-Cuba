0ef4caac7ed2ae76f4461a6157ca0edcd9f7d95e
Lerne <luisernestoserrras@gmail.com>
2024-06-16 21:20:45 -0400

    update json 16/06

--- csv_to_json.py
+++ csv_to_json.py
@@ -1,13 +1,13 @@
 import pandas as pd
 import json
-
+orient = ['dict','list','series','split','records','index']#Distintos tipos de orient para la generacion del diccionario 
 def csv_to_json(ruta_csv, ruta_json):
     try:
         # Leer el archivo CSV en un DataFrame
         df = pd.read_csv(ruta_csv)
 
         # Generando el diccionario a partir del DataFrame completo
-        titanic_json = df.to_dict(orient='records')
+        titanic_json = df.to_dict(orient=orient[4])
 
         # Guardando en archivo Json
         with open(ruta_json, 'w') as archivo_json:
@@ -17,7 +17,7 @@ def csv_to_json(ruta_csv, ruta_json):
     except Exception as e:
         print(f"Error al procesar el archivo CSV: {e}")
 #Prueba:
-ruta_csv = 'data/csv/9.16-existencia-de-ganado-vacuno-segun-sexo-y-categorias-en-diciembre-31-.csv'
+ruta_csv = "data/csv/vacuno/9.19-entregas-a-sacrificio-de-ganado-vacuno.csv"
 ruta_json = 'result.json'
 csv_to_json(ruta_csv,ruta_json)
 
