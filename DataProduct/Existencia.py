ea296f6fabfbd6587effe6750fe19540ab23ec55
Lerne <luisernestoserrras@gmail.com>
2024-08-18 16:38:23 -0400

    containers/explanation

--- DataProduct/Existencia.py
+++ DataProduct/Existencia.py
@@ -1,15 +1,3 @@
 import streamlit as st
-from st_pages import add_page_title, get_nav_from_toml, hide_pages
-import json
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd 
 
 st.write("Existencia")
-
-with open('inventario_ganado.json',encoding="utf8") as json_data: 
-    data = json.load(json_data)  
-    
-df = pd.DataFrame(data)
-print(df)
-st.bar_chart(df[Total].keys())
--- DataProduct/Rendimiento_Produccion.py
+++ DataProduct/Rendimiento_Produccion.py
@@ -1,5 +1,1 @@
-import streamlit as st
-import pandas as pd
-import numpy as np
-import json
-import plotly.express as px
+
