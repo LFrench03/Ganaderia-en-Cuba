1488c801e5322cce957dbef13b03876b85c669ae
Lerne <luisernestoserrras@gmail.com>
2024-09-04 04:34:50 -0400

    ident

--- Historia/historia.py
+++ Historia/historia.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from streamlit_timeline import timeline
-
-st.set_page_config(page_title="Timeline Example", layout="wide")
+st.logo(image="brand/PNG/Identificador principal.png")
+st.set_page_config(page_title="Timeline Ganaderia", layout="wide")
 
 with open("events.json", "r") as f:
     data = f.read()
